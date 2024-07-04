3355a5491b0c3a0243d51a360a1ae71e5b27b778
HAMID <HAMID@LAPTOP-3GA36M4V>
2024-07-04 23:22:27 +0530

    created by fareedh

---
+++ JavaBasics/src/DayTwo/IfelseDemo.java
@@ -0,0 +1,19 @@
+package DayTwo;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+		
+		int age = 19;
+		if (age>=18)
+		System.out.println("you are eligible for voting");
+		
+		else
+			
+			System.out.println("sorry!you need to wait more");
+		System.out.println("if else demonstration");
+		
+
+	}
+
+}
